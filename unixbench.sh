db7d1d138febb67d2ecb950235394e9a32c69300
Zhiqing Tang <root@tzq.me>
2018-03-17 01:41:05 +0800

    first commit

---
+++ unixbench.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+#==============================================================#
+#   Description:  Unixbench script                             #
+#   Author: root                          #
+#   Intro:  https://unixbench.org                      #
+#==============================================================#
+cur_dir=/opt/unixbench
+
+# Check System
+[[ $EUID -ne 0 ]] && echo 'Error: This script must be run as root!' && exit 1
+[[ -f /etc/redhat-release ]] && os='centos'
+[[ ! -z "`egrep -i debian /etc/issue`" ]] && os='debian'
+[[ ! -z "`egrep -i ubuntu /etc/issue`" ]] && os='ubuntu'
+[[ "$os" == '' ]] && echo 'Error: Your system is not supported to run it!' && exit 1
+
+# Install necessary libaries
+if [ "$os" == 'centos' ]; then
+    yum -y install make automake gcc autoconf gcc-c++ time perl-Time-HiRes zip unzip
+else
+    apt-get -y update
+    apt-get -y install make automake gcc autoconf time perl zip unzip
+fi
+
+# Create new soft download dir
+mkdir -p ${cur_dir}
+cd ${cur_dir}
+
+# Download UnixBench5.1.3
+if [ -s UnixBench.zip ]; then
+    echo "UnixBench.zip [found]"
+else
+    echo "UnixBench.zip not found!!!download now..."
+    if ! wget -c --no-check-certificate https://raw.githubusercontent.com/zq/unixbench/master/UnixBench/UnixBench.zip; then
+        echo "Failed to download UnixBench.zip, please download it to ${cur_dir} directory manually and try again."
+        exit 1
+    fi
+fi
+unzip UnixBench.zip && rm -f UnixBench.zip
+
+#Run unixbench
+make
+./Run
+
+echo
+echo
+echo "======= Script description and score comparison completed! ======= "
+echo
+echo
